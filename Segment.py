400c0e209894b6c126b1a647c16f7937c5a9b700
Bogu Wei <boguwei@gmail.com>
2016-04-02 22:50:52 -0700

    added print and copy methods to Segment class

--- Segment.py
+++ Segment.py
@@ -9,3 +9,11 @@ class Segment:
         self.x = x
         self.y = y
         self.z = z
+
+    def copyState(self, otherSegment):
+        self.x = otherSegment.x
+        self.y = otherSegment.y
+        self.z = otherSegment.z
+
+    def printSegment(self):
+        print('[ ',str(self.x),' ',str(self.y),' ',str(self.z),' ]')
